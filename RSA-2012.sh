dd45799cbac4b57840a25cb7b51f36ae744e7263
zoggy <zoggy@bat8.org>
2012-12-06 19:03:32 +0100

    follow stog changes

--- RSA-2012.sh
+++ RSA-2012.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-./vlmc-exp -o rsa_2012.R -l 250000 -d 25 laws/comb_log.cmxs laws/comb_fact.cmxs laws/expo.cmxs
-./vlmc-exp -o rsa_2012_2.R -l 250000 -d 25 laws/comb_fact.cmxs laws/expo.cmxs
+./vlmc-exp -o rsa_2012.R -l 250000 -d 25 --cores 5 --measure-step 100 laws/comb_log.cmxs laws/expo.cmxs laws/comb_fact.cmxs
+#./vlmc-exp -o rsa_2012_2.R -l 250000 -d 25 --cores 5 --measure-step 100 laws/comb_fact.cmxs laws/expo.cmxs
