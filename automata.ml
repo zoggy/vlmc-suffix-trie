36ab936f5149bf3bb145886555adfd34948ac230
zoggy <zoggy@bat8.org>
2013-04-17 16:59:18 +0200

    begin context tree completion

--- automata.ml
+++ automata.ml
@@ -2,9 +2,9 @@
 
 type 'a context = 'a array
 
-type 'a context_tree =
-  Context of 'a context
-| Node of 'a context_tree array
+type 'a tree =
+  Context of 'a
+| Node of 'a tree array
 
 
 let dot_of_context_tree f_sym f t =
@@ -33,44 +33,112 @@ let dot_of_context_tree f_sym f t =
   Buffer.contents b
 ;;
 
-type ('sym, 'a) automata_context = {
-    ac_path : 'sym list ;
+
+module OrderedIntList = struct type t = int list let compare = Pervasives.compare end
+(*module ACMap = Map.Make (OrderedIntList)*)
+module ACSet = Set.Make (OrderedIntList)
+
+type 'a automata_context = {
+    ac_path : int list;
     ac_ctx : 'a context ;
+    ac_next : int list context ;
+    ac_prev : ACSet.t ;
   }
 
-let automata_contexts_of_context_tree t =
-  let rec iter acc path = function
+let automata_context_tree t =
+  let rec map acc path = function
     Context c ->
       let ac =
         { ac_path = List.rev path ;
           ac_ctx = c ;
+          ac_next = Array.make (Array.length c) [] ;
+          ac_prev = ACSet.empty ;
         }
       in
-      ac :: acc
+      (Context ac, ac :: acc)
   | Node t ->
-     let (acc, _) =
+     let (trees, acc, _) =
         Array.fold_left
-          (fun (acc, i) n ->
-             let acc = iter acc (i :: path) n in
-             (acc, i+1)
+          (fun (trees, acc, i) n ->
+             let (tree, acc) = map acc (i :: path) n in
+             (tree :: trees, acc, i+1)
           )
-          (acc, 0)
+          ([], acc, 0)
           t
       in
-      acc
+      (Node (Array.of_list trees), acc)
   in
-  iter [] [] t
+  map [] [] t
 ;;
 
 let sort_automata_context_by_path_size =
   let compare ac1 ac2 =
-    match List.length ac1.ac_path - List.length ac2.ac_path with
-      0 -> Pervasives.compare ac1.ac_path ac2.ac_path
+    match List.length ac2.ac_path - List.length ac1.ac_path with
+      0 -> Pervasives.compare ac2.ac_path ac1.ac_path
     | n -> n
   in
-  fun l -> List.sort compare
+  fun l -> List.sort compare l
 ;;
 
+exception Need_break
+
+let find_context =
+  let rec iter = function
+    ([], Context c) -> c
+  | (_ :: _, Context c) -> c
+  | ([], Node _) -> raise Not_found
+  | (h :: q, Node t) ->  iter (q, t.(h))
+  in
+  fun path tree -> iter (path, tree)
+;;
+
+let replace_context tree ac ac2 =
+  ignore(ac=ac2);
+  let rec map = function
+    ([], Context _) -> Context ac2
+  | (_ :: _, Context _) -> assert false
+  | ([], Node _) -> assert false
+  | (h :: q, Node t) ->
+      let a = Array.copy t in
+      let th = map (q, t.(h)) in
+      a.(h) <- th;
+      Node a
+  in
+  map (ac.ac_path, tree)
+;;
+
+let complement_contexts =
+  let rec iter tree = function
+    [] -> tree
+  | ac :: q ->
+      try
+        let f (tree, i) _ =
+          let path = i :: ac.ac_path in
+          let ac_target =
+            try find_context path tree
+            with Not_found -> raise Need_break
+          in
+          let ac2_next = Array.copy ac.ac_next in
+          ac2_next.(i) <- ac_target.ac_path ;
+          let ac2 = { ac with ac_next = ac2_next } in
+          let ac_target2 = { ac_target with ac_prev = ACSet.add ac.ac_path ac_target.ac_prev } in
+          let tree = replace_context tree ac ac2 in
+          let tree = replace_context tree ac_target ac_target2 in
+          (tree, i+1)
+        in
+        let (tree, _) = Array.fold_left f (tree, 0) ac.ac_ctx in
+        tree
+      with
+        Need_break -> assert false
+  in
+  fun ctxs tree ->
+    iter tree ctxs
+;;
+
+let complement_context_tree t =
+  let (tree, contexts) = automata_context_tree t in
+  let contexts = sort_automata_context_by_path_size contexts in
+  complement_contexts contexts tree
 
 
 
@@ -94,5 +162,5 @@ let test_tree =
   |]
 ;;
 
-let dot = dot_of_context_tree string_of_int (fun _ -> "point", "") test_tree;;
+let dot = dot_of_context_tree string_of_int (fun _ -> "triangle", "") test_tree;;
 print_endline dot;;
