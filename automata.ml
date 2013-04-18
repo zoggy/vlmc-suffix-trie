dda0406ec8cbc1271f87c7217ff32c095b83681a
zoggy <zoggy@bat8.org>
2013-04-18 09:29:09 +0200

    complement seems to work on an example

--- automata.ml
+++ automata.ml
@@ -64,7 +64,7 @@ let dot_of_automata_context_tree f_sym t =
          | _ ->
              Printf.bprintf b "%s -> %s [ label=%s, style=dashed, constraint=false];\n"
                (id_of_path ac.ac_path)
-               (id_of_path (List.rev path)) (f_sym i)
+               (id_of_path path) (f_sym i)
       )
       ac.ac_next;
     "triangle", ""
@@ -120,70 +120,100 @@ let find_context =
   fun ?(exact=false) path tree -> iter exact (path, tree)
 ;;
 
-let replace_context tree ac ac2 =
+let map tree f =
+  let rec map path = function
+    Context c -> f (List.rev path) c
+  | Node t ->
+      let a =
+        Array.mapi
+          (fun i t -> map (i::path) t)
+          t
+      in
+      Node a
+  in
+  map [] tree
+;;
+
+let replace_context tree path f =
   let rec map = function
-    ([], Context _) -> Context ac2
+    ([], Context c) -> f c
   | (_ :: _, Context _) -> assert false
   | ([], Node _) -> assert false
   | (h :: q, Node t) ->
+      let a = Array.mapi
+        (fun i x -> if i = h then map(q, t.(h)) else x)
+        t
+      in
+      Node a
+(*
       let a = Array.copy t in
       let th = map (q, t.(h)) in
       a.(h) <- th;
       Node a
+*)
   in
-  map (ac.ac_path, tree)
+  map (path, tree)
 ;;
 
+let string_of_path path = String.concat "-" (List.map string_of_int path);;
+
 let break tree path =
-  let rec iter added tree path =
-    match added with
-      _ :: _ -> (tree, added)
-    | [] ->
-        match (path, tree) with
-          ([], Context ac) ->
-            let new_contexts =
-              Array.mapi
-                (fun i _ ->
-                   let new_ac =
-                     {
-                       ac_path = ac.ac_path @ [i] ;
-                       ac_ctx = ac.ac_ctx ;
-                       ac_next = Array.map (fun _ -> []) ac.ac_ctx ;
-                       ac_prev = ACSet.empty ;
-                     }
-                   in
-                   new_ac
-                )
-                ac.ac_ctx
-            in
-            let ctx_nodes = Array.map (fun ac -> Context ac) new_contexts in
-            (Node ctx_nodes, Array.to_list new_contexts)
-        | (_ :: _, Context _) -> assert false
-        | ([], Node _) -> assert false
-        | (h :: q, Node t) ->
-            let a = Array.copy t in
-            let (th, new_contexts) = iter added t.(h) q in
-            a.(h) <- th;
-            (Node a, new_contexts)
+  let added = ref [] in
+  let old_ac = find_context ~exact: true path tree in
+  let f ac =
+    let new_contexts =
+      Array.mapi
+        (fun i _ ->
+           let new_ac =
+             {
+               ac_path = ac.ac_path @ [i] ;
+               ac_ctx = ac.ac_ctx ;
+               ac_next = Array.map (fun _ -> []) ac.ac_ctx ;
+               ac_prev = ACSet.empty ;
+             }
+           in
+           new_ac
+        )
+        ac.ac_ctx
+    in
+    added := Array.to_list new_contexts ;
+    let ctx_nodes = Array.map (fun ac -> Context ac) new_contexts in
+    Node ctx_nodes
   in
-  let ac = find_context ~exact: true path tree in
-  let (tree, added) = iter [] tree path in
+  let tree = replace_context tree path f in
   let f_prev prev_path tree =
     match List.rev prev_path with
       i :: p ->
         let prev_path = List.rev p in
-        let old_ac = find_context ~exact: true prev_path tree in
-        let new_ac_next = Array.copy old_ac.ac_next in
-        (* set new link: search first context matching i :: old_ac.ac_path *)
-        let new_target = find_context (i :: old_ac.ac_path) tree in
-        new_ac_next.(i) <- new_target.ac_path ;
-        let new_ac = { old_ac with ac_next = new_ac_next } in
-        replace_context tree old_ac new_ac
+        let f old_ac =
+          let new_next = Array.mapi
+            (fun j x ->
+               if j = i then
+                 (* set new link: search first context matching i :: old_ac.ac_path *)
+                 let ac = find_context (i :: old_ac.ac_path) tree in
+                 prerr_endline
+                   (Printf.sprintf "break: prev=%s, old_next=%s, new_next=%s"
+                     (string_of_path prev_path)
+                     (string_of_path old_ac.ac_next.(i))
+                     (string_of_path ac.ac_path)
+                   );
+                 ac.ac_path
+               else
+                 x
+            )
+            old_ac.ac_next
+          in
+          let new_ac = { old_ac with ac_next = new_next } in
+          Context new_ac
+        in
+        replace_context tree prev_path f
     | _ -> assert false
   in
-  let tree = ACSet.fold f_prev ac.ac_prev tree in
-  (tree, added)
+  let tree = ACSet.fold f_prev old_ac.ac_prev tree in
+  (tree, !added)
 ;;
+
+let gensym = let i = ref 0 in fun () -> incr i; !i;;
 
 let complement_contexts =
   let rec iter tree = function
@@ -191,21 +221,32 @@ let complement_contexts =
   | ac :: q ->
       try
         let dot = dot_of_automata_context_tree string_of_int tree in
-        file_of_string ~file: "/tmp/foo.dot" dot;
+        file_of_string ~file: ("/tmp/foo"^(string_of_int (gensym()))^".dot") dot;
         let f (tree, i) _ =
           let path = i :: ac.ac_path in
           let ac_target =
             try find_context path tree
             with Not_found -> raise Need_break
           in
-          let ac2_next = Array.copy ac.ac_next in
-          ac2_next.(i) <- ac_target.ac_path ;
-          let ac2 = { ac with ac_next = ac2_next } in
-          let ac_target2 =
-            { ac_target with ac_prev = ACSet.add (ac.ac_path @ [i]) ac_target.ac_prev }
+          let tree = replace_context tree ac.ac_path
+            (fun ac ->
+               let ac2 =
+                 { ac with
+                   ac_next = Array.mapi
+                     (fun j x -> if j = i then ac_target.ac_path else x) ac.ac_next
+                 }
+               in
+               Context ac2
+            )
           in
-          let tree = replace_context tree ac ac2 in
-          let tree = replace_context tree ac_target ac_target2 in
+          let tree = replace_context tree ac_target.ac_path
+            (fun ac_target ->
+               let ac_target2 =
+                 { ac_target with ac_prev = ACSet.add (ac.ac_path @ [i]) ac_target.ac_prev }
+               in
+               Context ac_target2
+            )
+          in
           (tree, i+1)
         in
         let (tree, _) = Array.fold_left f (tree, 0) ac.ac_ctx in
