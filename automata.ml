054ff2fa524d9917c7c51a032c98e9c2d570a688
zoggy <zoggy@bat8.org>
2013-04-18 10:14:13 +0200

    add complemented_tree structure + dot outputs

--- automata.ml
+++ automata.ml
@@ -6,6 +6,8 @@ let file_of_string ~file s =
   close_out oc
 (*/c==v=[File.file_of_string]=1.1====*)
 
+let () = Random.self_init();;
+
 type 'a context = 'a array
 
 type 'a tree =
@@ -13,26 +15,40 @@ type 'a tree =
 | Node of 'a tree array
 
 
-let dot_of_context_tree f_sym f t =
+let dot_of_context_tree ?(graphatts=[]) ?edgeatts f_sym f t =
   let b = Buffer.create 256 in
   let id_of_path l =
     "N"^(String.concat "_" (List.map f_sym l))
   in
-  Buffer.add_string b "digraph g {\nrankdir = TB;\n";
+  let string_of_atts atts =
+    String.concat ", "
+      (List.map (fun (att, v) -> Printf.sprintf "%s=%S" att v) atts)
+  in
+  Buffer.add_string b "digraph g {\nrankdir = TB;\nordering=\"out\";\n";
+  List.iter
+    (fun (att, v) -> Printf.bprintf b "%s = %S;\n" att v)
+    graphatts;
   let rec iter path = function
     Context c ->
-      let (shape, label) = f b c in
-      Printf.bprintf b "%s [ shape=%S, label=%S ];\n"
-        (id_of_path (List.rev path)) shape label
+      let path = List.rev path in
+      let atts = f b path c in
+      Printf.bprintf b "%s [ %s ];\n"
+        (id_of_path path) (string_of_atts atts)
   | Node t ->
       let id = id_of_path (List.rev path) in
       Printf.bprintf b "%s [ shape=point ];\n" id;
       Array.iteri
         (fun i n ->
-           let path = i :: path in
-           Printf.bprintf b "%s -> %s [ label=%S ];\n" id
-             (id_of_path (List.rev path)) (f_sym i);
-           iter path n
+           let ipath = i :: path in
+           let atts =
+              match edgeatts with
+                None -> []
+             | Some f -> f (List.rev path) (List.rev ipath)
+           in
+           let atts = ("label", f_sym i) :: atts in
+           Printf.bprintf b "%s -> %s [ %s];\n" id
+             (id_of_path (List.rev ipath)) (string_of_atts atts);
+           iter ipath n
         )
         t
   in
@@ -42,7 +58,7 @@ let dot_of_context_tree f_sym f t =
 ;;
 
 module OrderedIntList = struct type t = int list let compare = Pervasives.compare end
-(*module ACMap = Map.Make (OrderedIntList)*)
+module ACMap = Map.Make (OrderedIntList)
 module ACSet = Set.Make (OrderedIntList)
 
 type 'a automata_context = {
@@ -56,7 +72,7 @@ let dot_of_automata_context_tree f_sym t =
   let id_of_path l =
     "N"^(String.concat "_" (List.map f_sym l))
   in
-  let f b ac =
+  let f b _ ac =
     Array.iteri
       (fun i path ->
       match path with
@@ -67,7 +83,7 @@ let dot_of_automata_context_tree f_sym t =
                (id_of_path path) (f_sym i)
       )
       ac.ac_next;
-    "triangle", ""
+    ["shape", "triangle"; "label", ""]
   in
   dot_of_context_tree f_sym f t
 ;;
@@ -110,6 +126,15 @@ let sort_automata_context_by_path_size =
 
 exception Need_break
 
+let node_exists =
+  let rec iter = function
+    ([], _) -> true
+  | (_ :: _, Context _) -> false
+  | (h :: q, Node t) -> iter (q, t.(h))
+  in
+  fun tree path -> iter (path, tree)
+;;
+
 let find_context =
   let rec iter exact = function
     ([], Context c) -> c
@@ -120,7 +145,7 @@ let find_context =
   fun ?(exact=false) path tree -> iter exact (path, tree)
 ;;
 
-let map tree f =
+let map_tree tree f =
   let rec map path = function
     Context c -> f (List.rev path) c
   | Node t ->
@@ -189,15 +214,17 @@ let break tree path =
           let new_next = Array.mapi
             (fun j x ->
                if j = i then
-                 (* set new link: search first context matching i :: old_ac.ac_path *)
-                 let ac = find_context (i :: old_ac.ac_path) tree in
-                 prerr_endline
-                   (Printf.sprintf "break: prev=%s, old_next=%s, new_next=%s"
-                     (string_of_path prev_path)
-                     (string_of_path old_ac.ac_next.(i))
-                     (string_of_path ac.ac_path)
-                   );
-                 ac.ac_path
+                 begin
+                   (* set new link: search first context matching i :: old_ac.ac_path *)
+                   let ac = find_context (i :: old_ac.ac_path) tree in
+                   prerr_endline
+                     (Printf.sprintf "break: prev=%s, old_next=%s, new_next=%s"
+                      (string_of_path prev_path)
+                        (string_of_path old_ac.ac_next.(i))
+                        (string_of_path ac.ac_path)
+                     );
+                   ac.ac_path
+                 end
                else
                  x
             )
@@ -270,7 +297,55 @@ let complement_context_tree t =
   prerr_endline (Printf.sprintf "length(contexts)=%d" (List.length contexts));
   let contexts = sort_automata_context_by_path_size contexts in
   complement_contexts contexts tree
+;;
 
+type 'a complemented_context_tree =
+  { cct_map : int list array ACMap.t ;
+    cct_tree : 'a tree ;
+  }
+
+let dot_of_cct f_sym cct =
+  let id_of_path l =
+    "N"^(String.concat "_" (List.map f_sym l))
+  in
+  let f b path c =
+    let color = Printf.sprintf "/paired12/%d" (((Random.int 6) + 1) * 2) in
+    Array.iteri
+      (fun i next ->
+         match path with
+           [] -> ()
+         | _ ->
+             Printf.bprintf b "%s -> %s [ label=%s, style=dashed, constraint=false, color=%S, fontcolor=%S];\n"
+               (id_of_path path)
+               (id_of_path next) (f_sym i)
+               color color
+      )
+      (ACMap.find path cct.cct_map);
+    ["shape", "triangle" ; "color", color; "label", ""]
+  in
+  dot_of_context_tree f_sym f cct.cct_tree
+;;
+
+let complemented_context_tree tree =
+  let map = ref ACMap.empty in
+  let f path ac =
+    map := ACMap.add path ac.ac_next !map ;
+    Context ac.ac_ctx
+  in
+  let tree = map_tree tree f in
+  { cct_map = !map ;
+    cct_tree = tree ;
+  }
+;;
+
+let dot_of_tree_diff f_sym tree cct =
+  let edgeatts path ipath =
+    let color = if node_exists tree ipath then "black" else "red" in
+    ["color", color ; "fontcolor", color]
+  in
+  let f b path _ = ["shape", "point" ; "label", ""] in
+  dot_of_context_tree ~edgeatts f_sym f cct.cct_tree
+;;
 
 
 let test_tree =
@@ -290,7 +365,11 @@ let test_tree =
 ;;
 
 
-let dot = dot_of_context_tree string_of_int (fun _ _ -> "triangle", "") test_tree;;
+let dot = dot_of_context_tree string_of_int (fun _ _ _ -> ["shape","triangle"]) test_tree;;
 print_endline dot;;
 let t2 = complement_context_tree test_tree;;
 file_of_string ~file: "/tmp/result.dot" (dot_of_automata_context_tree string_of_int t2);;
+let cct = complemented_context_tree t2;;
+file_of_string ~file: "/tmp/cct.dot" (dot_of_cct string_of_int cct);;
+
+file_of_string ~file: "/tmp/diff.dot" (dot_of_tree_diff string_of_int test_tree cct);;
