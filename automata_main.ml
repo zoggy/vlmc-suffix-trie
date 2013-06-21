13491781f0439684aed747556260d08f2361be60
zoggy <zoggy@bat8.org>
2013-06-21 09:57:17 +0200

    automata: add option --max-iter

--- automata_main.ml
+++ automata_main.ml
@@ -2,25 +2,35 @@
 open Automata;;
 
 let main () =
-  if Array.length Sys.argv < 2 then
-    failwith (Printf.sprintf "Usage: %s file.tree" Sys.argv.(0));
-  let file = Sys.argv.(1) in
-  let spec_str = Automata.string_of_file file in
-  let spec = Automata.read_spec spec_str in
-  let tree = Automata.context_tree_of_spec spec in
-  let basename = Filename.chop_extension file in
-
-  let dot = Automata.dot_of_context_tree
-    spec.spec_sym (fun _ _ _ -> ["shape","triangle"; "label",""]) tree
+  let args = ref [] in
+  let max_iter = ref None in
+  let options =
+    [ "--max-iter", Arg.Int (fun n -> max_iter := Some n),
+      "n set max iteration number when complementing, default is 120" ;
+    ]
   in
-  Automata.file_of_string ~file: (basename^".dot") dot;
+  Arg.parse options
+    (fun s -> args := !args @ [s])
+    (Printf.sprintf "Usage: %s [options] file.tree\nwhere options are:" Sys.argv.(0));
+  match List.rev !args with
+    [] -> failwith "Please give at least one file"
+  | file :: _ ->
+      let spec_str = Automata.string_of_file file in
+      let spec = Automata.read_spec spec_str in
+      let tree = Automata.context_tree_of_spec spec in
+      let basename = Filename.chop_extension file in
 
-  let cct = Automata.complemented_context_tree tree in
-  Automata.file_of_string ~file: (basename^".cct.dot")
-    (Automata.dot_of_cct spec.spec_sym cct);
+      let dot = Automata.dot_of_context_tree
+        spec.spec_sym (fun _ _ _ -> ["shape","triangle"; "label",""]) tree
+      in
+      Automata.file_of_string ~file: (basename^".dot") dot;
 
-  Automata.file_of_string ~file: (basename^".diff.dot")
-    (Automata.dot_of_tree_diff spec.spec_sym tree cct)
+      let cct = Automata.complemented_context_tree ?max_iter: !max_iter tree in
+      Automata.file_of_string ~file: (basename^".cct.dot")
+        (Automata.dot_of_cct spec.spec_sym cct);
+
+      Automata.file_of_string ~file: (basename^".diff.dot")
+        (Automata.dot_of_tree_diff spec.spec_sym tree cct)
 ;;
 
 try main ()
